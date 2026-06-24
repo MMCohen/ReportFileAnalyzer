8a8fcb2e9d199d4c45c4976140951e804ebf6c77
Mendel <MendelCohen770@gmail.com>
2026-06-24 17:46:38 +0300

    In the middle of building ProcessReports. basic version that works

--- Program.cs
+++ Program.cs
@@ -5,16 +5,16 @@ using System.Text;
 namespace reportFile
 {
     enum ReportType {
-        Intel,
-        Recon,
-        Analyze,
-        Collect
+        intel,
+        recon,
+        analyze,
+        collect
                     }
 
     enum Status {
-        Rejected,
-        Approved,
-        Pending
+        rejected,
+        approved,
+        pending
                 }
 
 
@@ -32,7 +32,7 @@ namespace reportFile
             Console.WriteLine(input);
         }
 
-        static string[]? LoadReports()
+        static string[]? LoadFile()
         {
             string[] filereports;
             try
@@ -52,7 +52,7 @@ namespace reportFile
                 return null;
             }
 
-
+            Console.WriteLine($"File loaded: {filereports.Length} lines found");
             return filereports;
 
         }
@@ -64,12 +64,111 @@ namespace reportFile
                 Console.WriteLine(data[i]);
             }
         }
+        
+
+        static void ProcessReports(string[] allLinesReport)
+        {
+            for (int i = 0; i < allLinesReport.Length; i++)
+            {
+                string Unit = "";
+                ReportType Report;
+                int Priority;
+                double Score;
+                Status status;
+
+                string invalidRecords = "invalid records: ";
+                bool isVAlid = true;
+
+                string[] oneLine = allLinesReport[i].Split(",");
+
+                if (oneLine.Length != 5)
+                {
+                    Console.WriteLine($"invalid record. line #:{i + 1}");
+                    isVAlid = false;
+                    continue;
+                }
+
+                oneLine = DeleteSpaces(oneLine);
+
+                // check that unit is not empty
+                if (oneLine[0].Length < 0)
+                {
+                    invalidRecords += "unit not enterd. ";
+                    isVAlid = false;
+                }
+                else
+                {
+                    Unit = oneLine[0];
+                }
+
+                if (!Enum.TryParse(oneLine[1].ToLower(), out Report))
+                {
+                    invalidRecords += "invalid ReportType. ";
+                    isVAlid = false;
+                }
+
+                if (!int.TryParse(oneLine[2], out Priority) || Priority < 1 || Priority > 5)
+                {
+                    invalidRecords += "invalid Priority. ";
+                    isVAlid = false;
+                }
+
+                if (!double.TryParse(oneLine[3], out Score) || Score < 0.0 || Score > 100.0)
+                {
+                    invalidRecords += "invalid Score. ";
+                    isVAlid = false;
+                }
+
+                if (!Enum.TryParse(oneLine[4].ToLower(), out status))
+                {
+                    isVAlid = false;
+                    invalidRecords += "invalid status. ";
+                }
+
+                if (!isVAlid)
+                {
+                    Console.WriteLine(invalidRecords);
+                    continue;
+                }
+
+                invalidRecords += 1;
+
+                //addToArrays(Unit, Report, Priority, Score, status);
+                Console.WriteLine($"Unit {Unit}, Report {Report}, Priority {Priority} , Score {Score}, status {status}");
+                 
+
+
+
+                // UNDONE:
+            }
+        }
+
+        static string[] DeleteSpaces(string[] oneReport)
+        {
+            string[] newReport = new string[5];
+
+            for (int i = 0; i < oneReport.Length; i++)
+            {
+                newReport[i] = oneReport[i].Trim();
+            }
+            return newReport;
+        }
+
         static void Main()
         {
             DebugPrinting("hello from main");
 
-            string[]? x = LoadReports();
-            
+            string[]? x = LoadFile();
+
+            ReportType[] reportTypeArrey;
+            Status[] StatusArrey;
+            string[] UnitNameArrey;
+            int[] PriorityArrey;
+            double[] ScoreArrey;
+
+            //ParsingReports();
+
+            ProcessReports(x);
 
         }
     }
