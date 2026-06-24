fd7c4d8bdc375596e330cfa1119a2d776c2631e2
Mendel <MendelCohen770@gmail.com>
2026-06-24 11:37:38 +0300

    basic structure

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+
+namespace reportFile
+{
+    class reportFileAnalayzer
+    {
+        static void DebugPrinting(string input) // for debug purpose
+        {
+            Console.WriteLine(input);
+        }
+
+        static List<string> LoadReports()
+        {
+            // need to check if file exist
+
+            string[] filereports = File.ReadAllLines("reports.txt");
+
+
+        }
+        static void Main()
+        {
+
+
+        }
+    }
+}
