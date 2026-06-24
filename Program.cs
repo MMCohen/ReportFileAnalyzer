8191e68411f6165838f13db4103f10965b8fa8df
Mendel <MendelCohen770@gmail.com>
2026-06-24 15:22:35 +0300

    add LoadReports func

--- Program.cs
+++ Program.cs
@@ -1,25 +1,75 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Text;
 
 namespace reportFile
 {
+    enum ReportType {
+        Intel,
+        Recon,
+        Analyze,
+        Collect
+                    }
+
+    enum Status {
+        Rejected,
+        Approved,
+        Pending
+                }
+
+
     class reportFileAnalayzer
     {
-        static void DebugPrinting(string input) // for debug purpose
+        static string FILEPATH = "reports.txt";
+
+
+        /// <summary>
+        /// printing func for debug purpose
+        /// </summary>
+        /// <param name="input"> the text you want tp print to the console </param>
+        static void DebugPrinting(string input)
         {
             Console.WriteLine(input);
         }
 
-        static List<string> LoadReports()
+        static string[]? LoadReports()
         {
-            // need to check if file exist
+            string[] filereports;
+            try
+            {
+                filereports = File.ReadAllLines(FILEPATH, encoding: Encoding.UTF8);
+                
+                if (filereports.Length == 0)
+                {
+                    Console.WriteLine("Error: File is empty.");
+                    return null;
+                }
+            }
 
-            string[] filereports = File.ReadAllLines("reports.txt");
+            catch (EndOfStreamException) // catch (FileNotFoundException)
+            {
+                Console.WriteLine($"Error: File {FILEPATH} is not found."); // TODO: change the file path for only the actaualy file name and not the full path.
+                return null;
+            }
 
 
+            return filereports;
+
+        }
+
+        static void ArreyPrint(string[] data)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine(data[i]);
+            }
         }
         static void Main()
         {
+            DebugPrinting("hello from main");
 
+            string[]? x = LoadReports();
+            
 
         }
     }
